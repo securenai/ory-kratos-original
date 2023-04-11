105a52646578cc6dc28e2f554fdd4cda3b508316
naishiuan <michaelnccu@gmail.com>
2023-04-11 22:49:09 +0800

    login update

--- pkg/ui/NodeInputDefault.tsx
+++ pkg/ui/NodeInputDefault.tsx
@@ -25,50 +25,65 @@ export function NodeInputDefault<T>(props: NodeInputProps) {
 
   // Render a generic text input field.
   return (
-    <TextInput
-      className="my-text-input"
-      style={{
-        border: isError ? "1px solid #F24867" : "none",
-        backgroundColor: "#37374F",
-        height: "44px",
-        color: "#fff",
-        caretColor: "#fff",
-        borderRadius: "8px",
-      }}
-      placeholder={node.meta.label?.text}
-      // title={node.meta.label?.text}
-      onClick={onClick}
-      onChange={(e) => {
-        setValue(e.target.value)
-      }}
-      type={attributes.type}
-      name={attributes.name}
-      value={value}
-      disabled={attributes.disabled || disabled}
-      help={node.messages.length > 0}
-      state={
-        node.messages.find(({ type }) => type === "error") ? "error" : undefined
-      }
-      subtitle={
-        <>
-          {node.messages.map(({ type, text, id }, k) => {
-            let displayText = text
-            if (text.includes("is missing")) {
-              // const field = text.split(" ")[1]
-              displayText = "This field is required, please fill it out."
-            }
-            return (
-              <span
-                key={`${id}-${k}`}
-                data-testid={`ui/message/${id}`}
-                style={{ color: "#F24867", fontSize: "13px" }}
-              >
-                {displayText}
-              </span>
-            )
-          })}
-        </>
-      }
-    />
+    <div style={{ position: "relative" }}>
+      <TextInput
+        className="my-text-input"
+        style={{
+          border: isError ? "1px solid #F24867" : "none",
+          backgroundColor: "#37374F",
+          height: "44px",
+          color: "#fff",
+          caretColor: "#fff",
+          borderRadius: "8px",
+        }}
+        placeholder={node.meta.label?.text}
+        // title={node.meta.label?.text}
+        onClick={onClick}
+        onChange={(e) => {
+          setValue(e.target.value)
+        }}
+        type={attributes.type}
+        name={attributes.name}
+        value={value}
+        disabled={attributes.disabled || disabled}
+        help={node.messages.length > 0}
+        state={
+          node.messages.find(({ type }) => type === "error")
+            ? "error"
+            : undefined
+        }
+        subtitle={
+          <>
+            {node.messages.map(({ type, text, id }, k) => {
+              let displayText = text
+              if (text.includes("is missing")) {
+                // const field = text.split(" ")[1]
+                displayText = "This field is required, please fill it out."
+              }
+              return (
+                <span
+                  key={`${id}-${k}`}
+                  data-testid={`ui/message/${id}`}
+                  style={{ color: "#F24867", fontSize: "13px" }}
+                >
+                  {displayText}
+                </span>
+              )
+            })}
+          </>
+        }
+      />
+      <span
+        style={{
+          color: "#CA4AE8",
+          fontSize: "16px",
+          fontFamily: "open sans",
+          position: "absolute",
+          top: "55px",
+        }}
+      >
+        {attributes.name === "password" ? "Forgot password?" : ""}
+      </span>
+    </div>
   )
 }
--- pkg/ui/NodeInputSubmit.tsx
+++ pkg/ui/NodeInputSubmit.tsx
@@ -1,6 +1,9 @@
 import { getNodeLabel } from "@ory/integrations/ui"
 import { Button } from "@ory/themes"
 
+import Apple from "../../public/images/login_icons/Apple"
+import Google from "../../public/images/login_icons/Google"
+
 import { NodeInputProps } from "./helpers"
 
 export function NodeInputSubmit<T>({
@@ -8,20 +11,61 @@ export function NodeInputSubmit<T>({
   attributes,
   disabled,
 }: NodeInputProps) {
-  // console.log(getNodeLabel(node))
+  console.log(getNodeLabel(node))
+  const type = getNodeLabel(node).includes("Google")
+    ? "Google"
+    : getNodeLabel(node).includes("Facebook")
+    ? "Apple"
+    : "default"
+  // console.log(attributes.name)
+  const defaultStyle = {
+    backgroundColor: "#A62BC3",
+    borderRadius: "8px",
+    height: "44px",
+  }
+
+  const GoogleBtn = {
+    width: "44px",
+    height: "44px",
+    backgroundColor: "transparent",
+    position: "absolute",
+    marginTop: "320px",
+    paddingTop: "8px",
+  }
+
+  const AppleBtn = {
+    width: "44px",
+    height: "44px",
+    backgroundColor: "transparent",
+    position: "absolute",
+    marginLeft: "70px",
+    marginTop: "320px",
+    paddingTop: "8px",
+  }
+
   return (
     <>
       <Button
-        style={{
-          backgroundColor: "#A62BC3",
-          borderRadius: "8px",
-          height: "44px",
-        }}
+        style={
+          type === "Google"
+            ? GoogleBtn
+            : type === "Apple"
+            ? AppleBtn
+            : defaultStyle
+        }
         name={attributes.name}
         value={attributes.value || ""}
         disabled={attributes.disabled || disabled}
       >
-        {getNodeLabel(node) === "Sign in" ? "Login" : getNodeLabel(node)}
+        {getNodeLabel(node) === "Sign in" ? (
+          "Login"
+        ) : getNodeLabel(node).includes("Google") ? (
+          <Google />
+        ) : getNodeLabel(node).includes("Facebook") ? (
+          <Apple />
+        ) : (
+          getNodeLabel(node)
+        )}
       </Button>
     </>
   )
